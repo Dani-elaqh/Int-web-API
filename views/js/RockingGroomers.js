ae894936e8ee6abc6ee3bce737c696bc53301e9c
Dani-elaqh <ale.quintanillah@gmail.com>
2021-01-03 02:58:03 +0000

    calculator

--- views/js/RockingGroomers.js
+++ views/js/RockingGroomers.js
@@ -1,4 +1,4 @@
-// This function is in case the prodcut can be delivered
+// This function is in case the product can be delivered
 // items (depending on the value of bShowDel)
 function highlightDelivery(idTable, bShowDel) {
     // if bShowDel is true, then we're show delivery, if not nothing to show
@@ -16,6 +16,25 @@ function highlightDelivery(idTable, bShowDel) {
             };
         };
     };
+};
+
+function calculateBill(idMenuTable) {
+    var fBillTotal = 0.0;
+    var i = 0;
+    var aCBTags = document.querySelectorAll('input');
+    for (i = 0; i < aCBTags.length; i++) {
+        // is this menu item selected? it is if the checkbox is checked
+        if (aCBTags[i].checked) {
+            // get the checkbox' parent table row
+            var oTR = getParentTag(aCBTags[i], 'TR');
+            // retrieve the price from the price column, which is the third column in the table
+            var oTDPrice = oTR.getElementsByTagName('TD')[2];
+            // the first child text node of the column contains the price
+            fBillTotal += parseFloat(oTDPrice.firstChild.data);
+        };
+    };
+    // return the price as a decimal number with 2 decimal places
+    return Math.round(fBillTotal * 100.0) / 100.0;
 };
 // Utility function for getting the parent tag of a given tag
 // but only of a certain type (i.e. a TR, a TABLE, etc.)
